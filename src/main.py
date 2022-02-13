8ff4cebad076c3a1e60004896774c1a5d35a35cb
jem2000 <justinem2000@gmail.com>
2022-02-13 18:43:22 -0500

    Added Home menu

--- src/main.py
+++ src/main.py
@@ -8,6 +8,30 @@ def app_startup():
     return char
 
 
+def home(user):
+    creds = user.get('credentials')
+    creds_string = "Your current roles are: \n"
+    for key in creds:
+        if creds[key] is True:
+            creds_string += ("  " + key.capitalize() + " -> Press " + key[0].capitalize() + " to access this role's "
+                                                                                            "functions")
+    print(creds_string)
+    valid = False
+    while not valid:
+        role = WConio2.getkey()
+        if role == "P":
+            print("Accessing patient menu")
+            valid = True
+        elif role == "D":
+            print("Accessing doctor menu")
+            valid = True
+        elif role == "A":
+            print("Accessing admin menu")
+            valid = True
+        else:
+            print("Invalid selection, please try again")
+
+
 if __name__ == '__main__':
     print("Welcome to Justin's Healthcare App!")
     logged_in = False
@@ -21,7 +45,6 @@ if __name__ == '__main__':
         else:
             print("Invalid selection")
 
-        if logged_in is not False:
-            current_user = logged_in
-            print("Success")
-            break
+    current_user = logged_in
+    print("Success")
+    home(current_user)
