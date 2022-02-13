71916183072c614224ff1a39a8160414256891c0
jem2000 <justinem2000@gmail.com>
2022-02-13 17:06:53 -0500

    Can now create user

---
+++ src/authentication.py
@@ -0,0 +1,69 @@
+import WConio2  # Only works on Windows!
+import pymongo
+import datetime
+
+client = pymongo.MongoClient(
+    "mongodb+srv://USERNAME:PASSWORD@cluster0.pkk8t.mongodb.net/CLIENTNAME?retryWrites=true&w=majority")
+
+db = client["Test1"]
+col = db["Collection1"]
+
+
+def login():
+    print("Logging in")
+    username = input("Please enter your username: ")
+
+
+def create_user():
+    print("Creating new user")
+    username = input("Please enter your username: ")
+    existing_name = col.find_one({'username': username})
+    if existing_name is not None:
+        valid = False
+        while not valid:
+            username = input("Name already taken, try a new name or enter 'B' to go back: ")
+            existing_name = col.find_one({'username': username})
+            if username == "B":
+                return False
+            if existing_name is None:
+                valid = True
+
+    password = input("Please enter your password: ")
+    is_doctor = False
+    is_admin = False
+    is_patient = False
+    finished_credentials = False
+    while not finished_credentials:
+        print("Select account credentials: Enter 'P' for patient, 'D' for doctor, or 'A' for admin ")
+        account_type = WConio2.getkey()
+        if account_type == "P" or account_type == 'D' or account_type == 'A':
+            if account_type == "P":
+                is_patient = True
+            elif account_type == "D":
+                is_doctor = True
+            elif account_type == "A":
+                is_admin = True
+            print("Would you like to add additional credentials Y/N? ")
+            additional = WConio2.getkey()
+            while additional != 'Y' and additional != 'N':
+                print("Try again, would you like to add additional credentials Y/N? ")
+                additional = WConio2.getkey()
+            if additional == 'N':
+                finished_credentials = True
+        else:
+            print("Invalid selection, try again")
+    credentials = {
+        "patient": is_patient,
+        "doctor": is_doctor,
+        "admin": is_admin
+    }
+    user_info = {
+        "username": username,
+        "password": password,
+        "credentials": credentials,
+        "account_creation_time": datetime.datetime.utcnow()
+    }
+
+    col.insert_one(user_info)
+
+    return True
--- src/main.py
+++ src/main.py
@@ -1,16 +1,24 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import WConio2  # Only works on Windows!
+import authentication as auth
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def app_startup():
+    print("Press 'L' to login or 'C' to create a new account")
+    char = WConio2.getkey()
+    return char
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print("Welcome to Justin's Healthcare App!")
+    logged_in = False
+    while not logged_in:
+        choice = app_startup()
+        if choice == "L":
+            logged_in = auth.login()
+        elif choice == "C":
+            logged_in = auth.create_user()
+        else:
+            print("Invalid selection")
+            app_startup()
+        if logged_in is False:
+            print("Authentication failed ")
