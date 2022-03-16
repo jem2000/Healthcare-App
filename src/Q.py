49dabadfbde821f1ee7ab49b08d6d33c1e40be00
jem2000 <justinem2000@gmail.com>
2022-03-16 13:05:14 -0400

    Queue second draft

--- src/Q.py
+++ src/Q.py
@@ -1,7 +1,4 @@
 import time
-import multiprocessing as mp
-from multiprocessing import Pool
-import _thread
 import threading
 import queue
 
@@ -10,18 +7,25 @@ class Queue:
     def __init__(self, max_processes):
         self.max_processes = max_processes
         self.q = queue.Queue()
+        self.available_processes = max_processes
 
-    def run(self, thread_no):
+    def run(self, thread_num):
         while True:
-            task = self.q.get()
-            square(3)
+            task, task_args = self.q.get()
+            task(task_args)
             self.q.task_done()
-            print(f'Thread #{thread_no} is doing task #{task} in the queue.')
+            print(f'Thread #{thread_num} is doing task #{task} in the queue.')
 
-    def start_threads(self):
-        for i in range(self.max_processes):
-            worker = threading.Thread(target=self.run, args=(i,), daemon=True)
+    def start_thread(self):
+        if self.available_processes > 0:
+            self.available_processes -= 1
+            worker = threading.Thread(target=self.run, args=(self.available_processes,), daemon=True)
             worker.start()
+        else:
+            print("All threads are being used")
+
+    def add_to_q(self, task, task_args):
+        self.q.put((task, task_args))
 
 
 def square(x):
@@ -35,9 +39,12 @@ def cube(x):
 
 
 if __name__ == '__main__':
-    Q = Queue(3)
-    Q.start_threads()
-    for j in range(10):
-        Q.q.put(j)
+    Q = Queue(max_processes=4)
+    for i in range(4):
+        Q.start_thread()
+    for j in range(3):
+        Q.add_to_q(square, 3)
+    for j in range(3):
+        Q.add_to_q(cube, 2)
 
     Q.q.join()
